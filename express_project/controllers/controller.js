a8aaf8224eca7354318379a927b90e4e2226c7d6
shubhcandela <shubham.shukla@candelatech.com>
2023-08-22 22:43:07 +0530

    added routes for controlling player from client side

--- express_project/controllers/controller.js
+++ express_project/controllers/controller.js
@@ -1,5 +1,8 @@
 // import data from "../data.js";
 import path from "path"
+let deviceData = {
+    
+}
 let data = {}
 let i = 0
 export const dashRouting = (req,res)=>{
@@ -19,5 +22,50 @@ export const postData = (req,res) =>{
     console.log(datatoadd)
     data = datatoadd
     res.send(data)
+}
 
+export const setDevice = (req,res) =>{
+    const {resource,state} = req.body
+    if(deviceData[resource] != undefined){
+        deviceData[resource]["state"] = state
+    }else{
+        deviceData[resource] ={
+            "state":state,
+            "stats":[]
+        }
+    }
+    console.log(deviceData)
+    res.send(deviceData)
+}
+
+export const getState = (req,res) =>{
+    try{
+        console.log(req.params.resource)
+    const key = req.params.resource
+    if(deviceData[key] != undefined){
+        console.log(deviceData[key])
+        res.send(deviceData[key]["state"]);
+    }else{
+        console.log(key+Object.keys(deviceData))
+
+        res.send("NA");
+    }}
+    catch{
+        console.log("errorrrrr")
+        res.send("error occured")
+    }
+}
+
+export const postDeviceData = (req,res)=>{
+    const {resource,data} = req.body
+    if(deviceData[resource] != undefined){
+        deviceData[resource]["stats"].push(data)
+        res.send("data posted")
+    }else{
+        res.send("NO Resorce Is Present")
+    }
+}
+
+export const getDeviceData = (req,res)=>{
+    res.send(deviceData)
 }
--- express_project/routes/route.js
+++ express_project/routes/route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {dashRouting,getData,postData} from "../controllers/controller.js"
+import {dashRouting,getData,getDeviceData,getState,postData, postDeviceData, setDevice} from "../controllers/controller.js"
 
 //router object
 const router = express.Router();
@@ -15,5 +15,14 @@ router.get('/data',getData);
 router.post('/data', postData);
 
 //Set state || POST
+router.post('/setstate', setDevice );
 
+//get status || GET
+router.get('/getstate/:resource', getState);
+
+//post data || POST
+router.post('/setdata', postDeviceData);
+
+//get data || GET
+router.get('/getdata', getDeviceData);
 export default router
